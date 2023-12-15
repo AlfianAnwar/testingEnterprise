44dd79491a85ee795a8be2fd89600b5b7785c3b3
malfian_a.i <malfian_a.i@S-EQ-NB-018.corp.bi.go.id>
2023-12-15 10:29:24 +0700

    TestUpload

---
+++ Test Suites/Testing.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>84a69bd9-227b-4000-8fe1-81c834b25b46</testSuiteGuid>
+   <testCaseLink>
+      <guid>01ea7a36-82c2-4f5d-8000-503b3f7da63b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Google</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
